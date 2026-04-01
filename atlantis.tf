dda1eb3b6313a18b42aa1a2e9bbd01af8a30baec
kaspar88 <liblik.kaspar7@gmail.com>
2026-04-01 15:19:59 +0300

    try to launch atlantis

---
+++ atlantis.tf
@@ -0,0 +1,121 @@
+resource "random_password" "atlantis_webhook_secret" {
+  length  = 40
+  special = false
+}
+
+resource "kubernetes_namespace" "atlantis" {
+  metadata {
+    name = "atlantis"
+  }
+
+  depends_on = [module.eks]
+}
+
+resource "kubernetes_secret" "atlantis_vcs" {
+  metadata {
+    name      = "atlantis-vcs"
+    namespace = kubernetes_namespace.atlantis.metadata[0].name
+  }
+
+  type = "Opaque"
+
+  string_data = {
+    token  = data.aws_secretsmanager_secret_version.github_token.secret_string
+    secret = random_password.atlantis_webhook_secret.result
+  }
+}
+
+resource "helm_release" "atlantis" {
+  name       = "atlantis"
+  namespace  = kubernetes_namespace.atlantis.metadata[0].name
+  repository = "https://runatlantis.github.io/helm-charts"
+  chart      = "atlantis"
+
+  values = [
+    yamlencode({
+      orgAllowlist = "github.com/kaspar88/*"
+
+      vcsSecretName = kubernetes_secret.atlantis_vcs.metadata[0].name
+
+      service = {
+        type       = "LoadBalancer"
+        port       = 80
+        targetPort = 4141
+      }
+
+      ingress = {
+        enabled = false
+      }
+
+      serviceAccount = {
+        create = true
+        name   = "atlantis"
+      }
+
+      volumeClaim = {
+        enabled     = true
+        dataStorage = "5Gi"
+      }
+
+      github = {
+        user = "atlantis-bot"
+      }
+
+      resources = {
+        requests = {
+          cpu    = "100m"
+          memory = "256Mi"
+        }
+        limits = {
+          cpu    = "500m"
+          memory = "512Mi"
+        }
+      }
+    })
+  ]
+
+  depends_on = [
+    module.eks,
+    kubernetes_namespace.atlantis,
+    kubernetes_secret.atlantis_vcs
+  ]
+}
+
+data "kubernetes_service" "atlantis" {
+  metadata {
+    name      = "atlantis"
+    namespace = kubernetes_namespace.atlantis.metadata[0].name
+  }
+
+  depends_on = [helm_release.atlantis]
+}
+
+locals {
+  atlantis_url = "http://${data.kubernetes_service.atlantis.status[0].load_balancer[0].ingress[0].hostname}"
+}
+
+resource "github_repository_webhook" "atlantis" {
+  repository = "atlantis"
+
+  active = true
+
+  events = [
+    "issue_comment",
+    "pull_request",
+    "pull_request_review",
+    "push",
+  ]
+
+  configuration {
+    url          = "${local.atlantis_url}/events"
+    content_type = "json"
+    insecure_ssl = false
+    secret       = random_password.atlantis_webhook_secret.result
+  }
+
+  depends_on = [helm_release.atlantis]
+}
+
+output "atlantis_url" {
+  value = local.atlantis_url
+}
