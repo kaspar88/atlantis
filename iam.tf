dc452d0f8883b6d6a8aa42bc987366b0f05bd049
kaspar88 <liblik.kaspar7@gmail.com>
2026-04-01 10:04:47 +0300

    fix

--- iam.tf
+++ iam.tf
@@ -7,11 +7,6 @@ resource "aws_iam_role" "eks_admin" {
   assume_role_policy = data.aws_iam_policy_document.eks_assume_role.json
 }
 
-resource "aws_iam_role_policy_attachment" "eks_admin_attach" {
-  role       = aws_iam_role.eks_admin.name
-  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
-}
-
 ############################
 # EKS READ-ONLY ROLE
 ############################
@@ -19,8 +14,3 @@ resource "aws_iam_role" "eks_readonly" {
   name               = "${local.cluster_name}-eks-readonly-role"
   assume_role_policy = data.aws_iam_policy_document.eks_assume_role.json
 }
-
-resource "aws_iam_role_policy_attachment" "eks_readonly_attach" {
-  role       = aws_iam_role.eks_readonly.name
-  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSReadOnlyAccess"
-} 
